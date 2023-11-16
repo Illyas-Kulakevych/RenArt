5ba95556014871e70638cd269b2b0edeb0e0bbb9
Ivan Ivash <ivanivash@MacBook-Pro-Ivan.local>
2023-11-16 14:42:47 +0100

    made header burger menu

---
+++ js/header.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const btnSub = document.querySelector(".header__menu-btn");
+const navLinks = document.querySelector(".header__menu");
+const socialLinks = document.querySelector(".header__social")
+
+btnSub.addEventListener("click", () => {
+    body.classList.toggle("fixed");
+    header.classList.toggle("responsive");
+
+})
+
+navLinks.addEventListener("click", () => {
+    body.classList.remove("fixed");
+    header.classList.remove("responsive");
+
+
+})
