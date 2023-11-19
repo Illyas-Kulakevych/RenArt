a286cb426fb39a85e78494db947ed795922824ba
as-vittsivskyi <147527745+as-vittsivskyi@users.noreply.github.com>
2023-11-19 13:45:24 +0000

    add solution for message task

---
+++ js-tasks/personalized-message.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+//Vittsivskyi
+function greet(name, owner) {
+    return (name === owner ? 'Hello boss' : 'Hello guest')
+}
