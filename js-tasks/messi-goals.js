bd170d3acbb00e2033fc5b0ee6109bc824790ab0
as-vittsivskyi <147527745+as-vittsivskyi@users.noreply.github.com>
2023-11-19 11:13:25 +0000

    Add Messi task from codewars

---
+++ js-tasks/messi-goals.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+//Vittsivskyi
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    const totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return totalGoals
+  }
