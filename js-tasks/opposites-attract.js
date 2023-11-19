cd711ff4d9e63c19a5cc4270908b504c21038470
Ivash94 <118127083+Ivash94@users.noreply.github.com>
2023-11-19 16:33:31 +0200

    Update opposites-attract.js

--- js-tasks/opposites-attract.js
+++ js-tasks/opposites-attract.js
@@ -4,3 +4,6 @@
 function lovefunc(flower1, flower2){
     return flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0
   }
+
+//Ivashkovych
+const lovefunc = (flower1, flower2) => (flower1 + flower2) % 2 === 1
