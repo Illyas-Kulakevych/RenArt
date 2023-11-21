fb46b40ff43c5a484cadf588e8a4b0ccd37e74d4
LEKa <olegmikadze@gmail.com>
2023-11-21 18:22:06 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -102,3 +102,8 @@ function otherAngle(a, b) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return dadYearsOld - sonYearsOld === 0 ? dadYearsOld : Math.abs(dadYearsOld - sonYearsOld * 2) 
 }
+
+//https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+function nthEven(n){
+  return (n-1)*2;
+}
