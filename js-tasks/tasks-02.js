20bdedf94338657215c1f3216471f24dcdf18a0f
LEKa <olegmikadze@gmail.com>
2023-11-21 18:24:16 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -107,3 +107,8 @@ function twiceAsOld(dadYearsOld, sonYearsOld) {
 function nthEven(n){
   return (n-1)*2;
 }
+
+//https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+function getRealFloor(n) {
+  return n < 0 ? n : n < 13 ? n - 1 : n -2 ;
+}
