99f47ff95b08c0de4d2a0f1d3713feebb333cb90
LEKa <olegmikadze@gmail.com>
2023-11-21 18:29:48 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -123,3 +123,8 @@ function past(h, m, s){
        + m * secondsInMinutes * millisecInSeconds
        + s * millisecInSeconds
 }
+
+//https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0
+}
