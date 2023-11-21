4ace139a6a4213ad7c30656e1cdf3bfef468f295
LEKa <olegmikadze@gmail.com>
2023-11-21 18:11:43 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -92,3 +92,8 @@ function cockroachSpeed(s) {
   
   return Math.floor(s * metersInKilometers * santimetersInMeters / (minutesInHour * secondsInMinute));
 }
+
+//https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+function otherAngle(a, b) {
+  return 180 - a - b;
+}
