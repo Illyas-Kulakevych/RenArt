cfda104260d033af6345fe33cf5191341ad018ee
LEKa <olegmikadze@gmail.com>
2023-11-21 17:57:16 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -65,3 +65,9 @@ function firstNonConsecutive(arr) {
   }
   return null;
 }
+
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i*2;
+}
