7dcbdbdde0a8086a4c59f99ce3aea3c94c6215b5
as-vittsivskyi <147527745+as-vittsivskyi@users.noreply.github.com>
2023-11-26 17:49:22 +0000

    Add js for main-news section

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener("partialsLoaded", () => {
     import("./header.js");
     import("./accordion.js");
     import("./artgallery__clock.js");
+    import ("./main-news.js");
   });
