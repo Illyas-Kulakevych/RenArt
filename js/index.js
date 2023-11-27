79bf392393622ad2e1cf09e4595f69a5b9646f32
geersann <148795588+geersann@users.noreply.github.com>
2023-11-27 23:58:03 +0000

    change to artgallery way

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 document.addEventListener("partialsLoaded", () => {
     import("./header.js");
-    import("./accordion.js");
-    import("./artgallery__clock.js");
     import ("./main-news.js");
   });
