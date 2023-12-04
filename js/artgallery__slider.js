fcac0e91fee1b563bb58a5a9d4bb0ccd0dbc082a
geersann <148795588+geersann@users.noreply.github.com>
2023-12-04 17:11:03 +0000

    change artgallery-slider js

--- js/artgallery__slider.js
+++ js/artgallery__slider.js
@@ -1,52 +1,54 @@
-const sliderImages = document.querySelectorAll('.slider__img'),
-    sliderLine = document.querySelector('.slider__line'),
-    sliderDots = document.querySelectorAll('.slider__dot'),
-    sliderBtnPrev = document.querySelector('.btn-prev'),
-    sliderBtnNext = document.querySelector('.btn-next');
+const sliderImages = document.querySelectorAll(".slider__img"),
+  sliderLine = document.querySelector(".slider__line"),
+  sliderDots = document.querySelectorAll(".slider__dot"),
+  sliderBtnPrev = document.querySelector(".btn-prev"),
+  sliderBtnNext = document.querySelector(".btn-next");
 
-let sliderWidth;   
+let sliderWidth = document.querySelector(".slider-container").offsetWidth;
 let sliderCount = 0;
 
-window.addEventListener('resize', showSlide);
+window.addEventListener("resize", showSlide);
 
-sliderBtnPrev.addEventListener('click', prevSlide);
-sliderBtnNext.addEventListener('click', nextSlide);
-    
+sliderBtnPrev.addEventListener("click", prevSlide);
+sliderBtnNext.addEventListener("click", nextSlide);
+
 function showSlide() {
-    sliderWidth =  document.querySelector('.slider-container').offsetWidth;
-    sliderLine.style.width = sliderWidth * sliderImages.length + "px"
-    sliderImages.forEach(item => item.style.width = sliderWidth + "px");
-    rollSlider();
+  sliderWidth = document.querySelector(".slider-container").offsetWidth;
+  sliderLine.style.width = `${sliderWidth * sliderImages.length}px`;
+  sliderImages.forEach((item) => (item.style.width = `${sliderWidth}px`));
+  rollSlider();
 }
 showSlide();
 
 function prevSlide() {
-    sliderCount--;
-    if(sliderCount < 0) sliderCount = sliderImages.length -1;
+  sliderCount--;
+  if (sliderCount < 0) sliderCount = sliderImages.length - 1;
 
-    rollSlider();
-    thisSlide(sliderCount);
+  rollSlider();
+  thisSlide(sliderCount);
 }
+
 function nextSlide() {
-    sliderCount ++;
-    if(sliderCount >= sliderImages.length) sliderCount = 0;
-    rollSlider();
-    thisSlide(sliderCount);
+  sliderCount++;
+  if (sliderCount >= sliderImages.length) sliderCount = 0;
+  rollSlider();
+  thisSlide(sliderCount);
 }
 
 function rollSlider() {
-    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
 }
 
 function thisSlide(index) {
-    sliderDots.forEach(item => item.classList.remove('active-dot'));
-    sliderDots[index].classList.add('active-dot');
+  sliderDots.forEach((item, i) =>
+    item.classList.toggle("active-dot", i === index),
+  );
 }
 
 sliderDots.forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-        sliderCount = index;
-        rollSlider();
-        thisSlide(sliderCount);
-    })
-})
+  dot.addEventListener("click", () => {
+    sliderCount = index;
+    rollSlider();
+    thisSlide(sliderCount);
+  });
+});
