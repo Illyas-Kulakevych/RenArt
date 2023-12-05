f692c77a730b8fd3c7ddeee6fc7a09ae88d47d0a
Ivan Ivash <ivanivash@MacBook-Pro-Ivan.local>
2023-12-05 20:24:07 +0100

    changed header color

--- js/header.js
+++ js/header.js
@@ -3,23 +3,19 @@ const header = document.querySelector(".header");
 const btnSub = document.querySelector(".header__menu-btn");
 const navLinks = document.querySelector(".header__menu");
 
-
 btnSub.addEventListener("click", () => {
-    body.classList.toggle("fixed");
-    header.classList.toggle("responsive");
-
-})
+  body.classList.toggle("fixed");
+  header.classList.toggle("responsive");
+});
 
 navLinks.addEventListener("click", () => {
-    body.classList.remove("fixed");
-    header.classList.remove("responsive");
-
-})
-window.addEventListener("resize", function() {
-    if (window.innerWidth > 991) header.classList.remove("responsive") ;
-    body.classList.remove("fixed");
-  });
-
+  body.classList.remove("fixed");
+  header.classList.remove("responsive");
+});
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 991) header.classList.remove("responsive");
+  body.classList.remove("fixed");
+});
 
 
 
