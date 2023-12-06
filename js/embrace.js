d2d6f5de0fca18d9a9d54321415e061387d502d4
LEKa <olegmikadze@gmail.com>
2023-12-06 18:04:13 +0000

    load illustrations with js

---
+++ js/embrace.js
@@ -0,0 +1,30 @@
+
+    setTimeout(async function(){
+      const response = await fetch('api/embrace.json');
+      const images = await response.json();
+  
+      displayImages(images);
+    }, 100);
+  
+    
+    function displayImages(imgs) {
+        console.log('IMAGES', imgs)
+      const illustrationContainer = document.querySelector(".embrace__illustration--wrapper");
+      let illustrationsHtml = '';
+      
+     
+      for (const image of imgs) {
+        illustrationsHtml += `
+            <img class='${image.class}' src='${image.src}' alt='${image.alt}'>
+        `;
+      }
+      if (illustrationContainer) {
+        illustrationContainer.innerHTML = illustrationsHtml;
+      } else {
+        console.error('Елемент не знайдений');
+      }
+  
+    };
+  
+  
+  
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
     import("./header.js");
-    import ("./main-news.js");
+    import("./main-news.js");
+    import("./embrace.js");
   });
